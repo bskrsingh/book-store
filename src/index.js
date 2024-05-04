4fe704431bfbc8f6fffd6bd740f7aa07573e1b93
unknown <bskrsingh@gmail.com>
2024-05-04 12:05:03 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />}>
+        <Route path="/" element={<Login />}>
         <Route index element={<Login />} />
-          <Route path="schoolDetails" element={<SchoolDetails />} />
+          <Route path="/schoolDetails" element={<SchoolDetails />} />
           </Route>
       </Routes>
     </BrowserRouter>
